015f1693ec6a2409feb24058f4c8a9694f39f499
Thiago <thiago_sl2@htomail.com>
2025-02-23 17:06:32 -0300

    feature: dockerFile

--- src/Chat/controllers/templates.js
+++ src/Chat/controllers/templates.js
@@ -1,10 +1,9 @@
 class templatesController {
     constructor() {
         this._dbConnectionTemplates = mongoose.createConnection(Connections.TEMPLATES)
-
-
         this._dbConnectionTemplates.on("connected", () => {
             console.log("Connected to the Templates database");
         });
     }
+    
 }
