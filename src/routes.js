7491ce999c0840dc38622d7f9465d88731802470
Thiago <thiago_sl2@htomail.com>
2025-03-05 16:56:16 -0300

    feature: security request

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import routesPolicy from './Policy/routes.js';
 import { routesBot, routesChat, routesMessage } from './Chat/routes.js';
+import { authorize } from './utils/middleware.js';
 
 const routes = express.Router();
 
-routes.use('/api/policy', routesPolicy);
-routes.use('/api/chat', routesChat);
-routes.use('/api/message', routesMessage);
-routes.use('/api/bot', routesBot);
+routes.use('/api/policy', authorize, routesPolicy);
+routes.use('/api/chat', authorize, routesChat);
+routes.use('/api/message', authorize, routesMessage);
+routes.use('/api/bot',authorize, routesBot);
 
 export default routes;
